be40d1375babfb1ef400d9654803dd4eb1925215
Yahweh's Delight <intricatesyllable@gmail.com>
2024-10-30 00:18:42 +0300

    shebang

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""Pagination helper function
+"""
+from typing import Tuple
+
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
@@ -20,5 +24,5 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
         (14, 21)
 """
     start = (page - 1) * page_size
-    end = start + page_size
+    end = (start + page_size)
     return (start, end)
