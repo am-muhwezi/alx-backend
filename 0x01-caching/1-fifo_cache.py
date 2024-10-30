c34ebe4ca810290766de4de3e3f31094e5aa6303
Yahweh's Delight <intricatesyllable@gmail.com>
2024-10-31 02:22:51 +0300

    First In First Out Algo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,18 +7,20 @@ from base_caching import BaseCaching
 class FIFOCache(BaseCaching):
     """ FIFO cache class """
     def __init__(self):
+        """ Initiliaze"""
         super().__init__()
+        self.order = []
 
     def put(self, key, item):
         if key is not None and item is not None:
+            if key not in self.cache_data:
+                self.order.append(key)
             self.cache_data[key] = item
 
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 first = list(self.cache_data.keys())[0]
                 del self.cache_data[first]
-                print("DISCARD: {}".format(first))
+                print("DISCARD:", (first))
 
     def get(self, key):
-        if key is None or key not in self.cache_data.get(key):
-            return None
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
