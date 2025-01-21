857e85febc5ba66cfcd327d29cc528442fcdca8f
Yahweh's Delight <michealphenh10@gmail.com>
2025-01-21 19:25:54 +0300

    Helper function

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python3
+"""Task 1: Simple pagination.
+"""
+
 import csv
 import math
-from typing import List
-from typing import Tuple
+from typing import List, Tuple
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple of size two containing a start index and an end index"""
-    return ((page - 1) * page_size, page * page_size)
+    """Retrieves the index range from a given page and page size.
+    """
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -27,7 +34,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Get page from dataset"""
-        assert(type(page == int) and type(page_size == int))
-        assert(page > 0) and (page_size > 0)
-        index_range = (page, page_size)
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
