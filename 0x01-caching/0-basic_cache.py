984e0d73e25bcaa7e3fe3948eaa38d32636d589c
Yahweh's Delight <michealphenh10@gmail.com>
2025-01-21 19:47:44 +0300

    0x01-caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,18 +1,26 @@
-#!/usr/bin/python3
-""" Basic cache"""
+#!/usr/bin/env python3
+
+'''Task 0: Basic dictionary
+'''
+
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Basic cache class """
+    '''A class `BasicCache` that inherits from `BaseCaching`
+       and is a caching system
+    '''
+
     def put(self, key, item):
-        """ Add an item in the cache"""
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        '''return the value in `self.cache_data` linked to `key`
+        '''
+
+        return self.cache_data.get(key, None)
--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,26 +1,37 @@
-#!/usr/bin/python3
-""" Basic cache"""
+#!/usr/bin/env python3
 
+'''Task 1: FIFO caching
+'''
+
+
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ FIFO cache class """
+    '''A class `FIFOCache` that inherits from
+       `BaseCaching` and is a caching system.
+    '''
+
     def __init__(self):
-        """ Initiliaze"""
         super().__init__()
-        self.order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        if key is not None and item is not None:
-            if key not in self.cache_data:
-                self.order.append(key)
-            self.cache_data[key] = item
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
 
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first = list(self.cache_data.keys())[0]
-                del self.cache_data[first]
-                print("DISCARD:", (first))
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
 
     def get(self, key):
+        '''return the value in `self.cache_data` linked to `key`
+        '''
         return self.cache_data.get(key, None)
